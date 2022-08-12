472696c988d6164b0dd2edef6209c97629f96872
Edmond <34758460+EdmondtheGreat@users.noreply.github.com>
2022-08-12 13:02:15 +0800

    Initial Commit

---
+++ Challenge1/Challenge1App.swift
@@ -0,0 +1,17 @@
+//
+//  Challenge1App.swift
+//  Challenge1
+//
+//  Created by OAA on 12/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct Challenge1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Challenge1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Challenge1
+//
+//  Created by OAA on 12/08/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
